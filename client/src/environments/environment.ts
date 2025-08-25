61274297d5191ef9aa265c0bd59e907364eadc50
brandon <bs.edwards22@gmail.com>
2025-08-24 20:42:54 -0500

    changed api path for dev also

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:32775/'   // your dev API
+//   apiUrl: 'https://localhost:32775/'   // your dev API
+  apiUrl: 'http://192.168.0.62:5000/'   // your dev API
 };
